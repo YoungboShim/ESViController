8de291bec22e8bbc4e4255813f2df90d70087081
Youngbo Shim <aram6207@kaist.ac.kr>
2022-11-04 16:26:10 +0900

    Update ViGEm Client test - sticks & triggers transferred

--- ESViController/MainWindow.xaml.cs
+++ ESViController/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Nefarius.ViGEm.Client;
+using System.Threading;
+using System.Diagnostics;
 
 namespace ESViController
 {
@@ -20,9 +23,70 @@ namespace ESViController
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread t;
+        XInputController ds4;
+
         public MainWindow()
         {
             InitializeComponent();
+            ds4 = new XInputController();
+        }
+
+        private void ViGEmTest()
+        {
+            // initializes the SDK instance
+            var client = new ViGEmClient();
+
+            // prepares a new DS4
+            var esPad = client.CreateDualShock4Controller();
+
+            // brings the DS4 online
+            esPad.Connect();
+
+            Debug.WriteLine("Start button clicked - ViGEm thread");
+
+            // recommended: run this in its own thread
+            while (true)
+            {
+                try
+                {
+                    if (ds4.connected)
+                    {
+                        ds4.Update();
+                        esPad.SetAxisValue(Nefarius.ViGEm.Client.Targets.DualShock4.DualShock4Axis.LeftThumbX, ds4.leftThumbX);
+                        esPad.SetAxisValue(Nefarius.ViGEm.Client.Targets.DualShock4.DualShock4Axis.LeftThumbY, ds4.leftThumbY);
+                        esPad.SetAxisValue(Nefarius.ViGEm.Client.Targets.DualShock4.DualShock4Axis.RightThumbX, ds4.rightThumbX);
+                        esPad.SetAxisValue(Nefarius.ViGEm.Client.Targets.DualShock4.DualShock4Axis.RightThumbY, ds4.rightThumbY);
+                        esPad.LeftTrigger = (byte)ds4.leftTrigger;
+                        esPad.RightTrigger = (byte)ds4.rightTrigger;
+                        Thread.Sleep(10);
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Not connected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Start button clicked - main thread");
+            t = new Thread(ViGEmTest);
+            t.Start();
+        }
+
+        private void buttonEnd_Click(object sender, RoutedEventArgs e)
+        {
+            if (t.IsAlive)
+            {
+                // Terminate thread somehow.
+            }
         }
     }
 }
---
+++ ESViController/XInputController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpDX.XInput;
+using System.Drawing;
+using System.Diagnostics;
+using Nefarius.ViGEm.Client.Utilities;
+
+namespace ESViController
+{
+    class XInputController
+    {
+        Controller controller;
+        Gamepad gamepad;
+        public bool connected = false;
+        public int deadband = 2500;
+        public byte leftThumbX, leftThumbY, rightThumbX, rightThumbY = 0;
+        public float leftTrigger, rightTrigger;
+
+        public XInputController()
+        {
+            var controllers = new[] { new Controller(UserIndex.One), new Controller(UserIndex.Two), new Controller(UserIndex.Three), new Controller(UserIndex.Four) };
+
+            foreach (var selectControler in controllers)
+            {
+                if (selectControler.IsConnected)
+                {
+                    controller = selectControler;
+                    break;
+                }
+            }
+
+            if (controller == null)
+                Debug.WriteLine("No controller installed");
+            else
+                connected = controller.IsConnected;
+        }
+
+        public void Update()
+        {
+            if (!connected)
+                return;
+
+            gamepad = controller.GetState().Gamepad;
+
+            leftThumbX = short2byte(gamepad.LeftThumbX);
+            leftThumbY = short2byte(-gamepad.LeftThumbY);
+            rightThumbX = short2byte(gamepad.RightThumbX);
+            rightThumbY = short2byte(-gamepad.RightThumbY);
+
+            leftTrigger = gamepad.LeftTrigger;
+            rightTrigger = gamepad.RightTrigger;
+        }
+
+        private byte short2byte(float val)
+        {
+            return (byte)(val / short.MaxValue * byte.MaxValue / 2f + byte.MaxValue / 2f);
+        }
+    }
+}
